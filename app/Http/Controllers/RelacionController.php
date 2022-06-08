88f40d278fe7596ac005a7e7dda61c069e62861f
Juan Pandales <karpus13@gmail.com>
2022-06-08 12:02:30 -0500

    Controlador para la imagen si tiene o no

--- app/Http/Controllers/RelacionController.php
+++ app/Http/Controllers/RelacionController.php
@@ -12,6 +12,19 @@ class RelacionController extends Controller
     public function index()
     {
         $productos = Producto::all();
+
+        //Inserción de imágenes en un producto
+
+        if($productos -> hasFile('imagen')){
+            if ($productos->image !=null){
+                Producto::disk('images')->delete($productos->image->path);
+                $productos->user->image->delete();
+            }
+            $productos->image()->create([
+                'path' => $productos->image->store('productos', 'images'),
+            ]);
+        }
+
         //Básica función Index para mostrar los registro "productos"
         return view('welcome', compact('productos'));
 }
