2f78b7eec56b9a813f5f3dfa81c6648239613fb7
Juan Pandales <karpus13@gmail.com>
2022-06-08 11:16:55 -0500

    Controlador para mostrar registros

--- app/Http/Controllers/RelacionController.php
+++ app/Http/Controllers/RelacionController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
+use App\Models\Tienda;
+
 use Illuminate\Http\Request;
 
 class RelacionController extends Controller
 {
-    //
+    public function index()
+    {
+        $productos = Producto::all();
+        //Básica función Index para mostrar los registro "productos"
+        return view('welcome', compact('productos'));
+}
 }
